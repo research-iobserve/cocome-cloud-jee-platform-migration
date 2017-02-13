894579869f085801bcc71594cbaa705c57a8d936
Reiner Jung <reiner.jung@email.uni-kiel.de>
2017-02-13 17:57:46 +0100

    cleanups

--- cocome-maven-project/cloud-logic-service/cloud-registry-service/src/main/java/org/cocome/cloud/registry/service/URIRegistryManager.java
+++ cocome-maven-project/cloud-logic-service/cloud-registry-service/src/main/java/org/cocome/cloud/registry/service/URIRegistryManager.java
@@ -2,7 +2,10 @@ package org.cocome.cloud.registry.service;
 
 import java.net.URI;
 import java.rmi.NotBoundException;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
@@ -10,34 +13,37 @@ import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.jws.WebService;
 
-@WebService(serviceName = "IURIRegistryManagerService", name = "IURIRegistryManager", endpointInterface = "org.cocome.cloud.registry.service.IURIRegistryManager", targetNamespace = "http://registry.webservice.logic.cocome.org/")
+@WebService(serviceName = "IURIRegistryManagerService",
+			name = "IURIRegistryManager",
+			endpointInterface = "org.cocome.cloud.registry.service.IURIRegistryManager",
+			targetNamespace = "http://registry.webservice.logic.cocome.org/")
 @Stateless
 public class URIRegistryManager implements IURIRegistryManager {
-
+	
 	@Inject
 	IRegistry<URI> registry;
 
 	@Override
-	public void rebind(final String name, final URI location) {
-		this.registry.rebind(name, location);
+	public void rebind(String name, URI location) {
+		registry.rebind(name, location);
 	}
 
 	@Override
-	public boolean unbind(final String name) {
-		return this.registry.unbind(name);
+	public boolean unbind(String name) {
+		return registry.unbind(name);
 	}
 
 	@Override
-	public URI lookup(final String name) throws NotBoundException {
-		return this.registry.lookup(name);
+	public URI lookup(String name) throws NotBoundException {
+		return registry.lookup(name);
 	}
 
 	@Override
 	public Set<RegistryEntry> getBoundNames() {
-		final Set<Entry<String, URI>> registryEntries = this.registry.getEntries();
-		final Set<RegistryEntry> entries = new LinkedHashSet<>(registryEntries.size(), 1);
-		for (final Entry<String, URI> entry : registryEntries) {
-			final RegistryEntry regEntry = new RegistryEntry();
+		Set<Entry<String, URI>> registryEntries = registry.getEntries();
+		Set<RegistryEntry> entries = new LinkedHashSet<>(registryEntries.size(), 1);
+		for (Entry<String, URI> entry : registryEntries) {
+			RegistryEntry regEntry = new RegistryEntry();
 			regEntry.setLocation(entry.getValue());
 			regEntry.setName(entry.getKey());
 			entries.add(regEntry);
